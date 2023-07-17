66375572ca92de88b13eb776ccfee288322f55e7
Choohan <skyarmymy@gmail.com>
2023-07-18 01:02:14 +0800

    newReply

--- resources/views/mail/newReplyAuthor.blade.php
+++ resources/views/mail/newReplyAuthor.blade.php
@@ -3,7 +3,7 @@
         <p style="white-space: pre-line">
             Dear {{ $user->name }},
 
-            We had received received your replies and will definitely notify you once you gotten a new message through this email!
+            We had received your replies and will definitely notify you once you gotten a new message through this email!
 
             <b>Check your mail at:</b>
             <a href="{{ route('sm.view', ['id'=>$mail->id]) }}">{{ route('sm.view', ['id'=>$mail->id]) }}</a>
